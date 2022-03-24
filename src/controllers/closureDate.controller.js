e3bca9951be59a49683265b887cb31a0a8c507b0
nguyentiendat12a8 <74580065+nguyentiendat12a8@users.noreply.github.com>
2022-03-25 00:30:00 +0700

    upload list closuredate

--- src/controllers/closureDate.controller.js
+++ src/controllers/closureDate.controller.js
@@ -2,17 +2,17 @@ const db = require('../models/index')
 const ClosureDate = db.closureDate
 const Department = db.department
 
-exports.createClousureDate = async  (req,res) =>{
-    try{
+exports.createClousureDate = async (req, res) => {
+    try {
         //10-2-2020
-        const department = await Department.findOne({departmentName: req.body.departmentName})
+        const department = await Department.findOne({ departmentName: req.body.departmentName })
         const closureDate = new ClosureDate({
             firstClosureDate: req.body.firstClosureDate,
             finalClosureDate: req.body.finalClosureDate,
             departmentID: department._id
         })
-        closureDate.save(err =>{
-            if(err) return res.status(500).send({
+        closureDate.save(err => {
+            if (err) return res.status(500).send({
                 errorCode: '500',
                 message: 'add closure date fail!'
             })
@@ -22,8 +22,38 @@ exports.createClousureDate = async  (req,res) =>{
             })
         })
     }
-    catch(err) {
+    catch (err) {
         console.log(err)
+    }
+}
+
+exports.listClosureDate = (req, res) => {
+    try {
+        ClosureDate.find({},async (err, listClosureDate) => {
+            if (err) return res.status(500).send({
+                errorCode: 500,
+                message: err
+            })
+            var listShow = []
+            for(i=0; i< listClosureDate.length; i++){
+                var departmentID = listClosureDate[i].departmentID
+                var department = await Department.findOne({ _id: departmentID })
+                var show = {
+                    firstClosureDate: listClosureDate[i].firstClosureDate,
+                    finalClosureDate: listClosureDate[i].finalClosureDate,
+                    departmentName: department.departmentName
+                }
+                console.log(show)
+                listShow.push(show)
+            }
+                
+            return res.status(200).send({
+                errorCode: 0,
+                data: listShow
+            })
+        })
+    } catch (error) {
+        console.log(error)
     }
 }
 
--- src/routers/closureDate.router.js
+++ src/routers/closureDate.router.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { createClousureDate } = require('../controllers/closureDate.controller');
+const { createClousureDate, listClosureDate } = require('../controllers/closureDate.controller');
 const {verifyToken, isAdmin } = require('../middlerwares/jwt.middleware');
 const router = express.Router();
 
 router.post('/create-closure-date',[verifyToken, isAdmin], createClousureDate)
+router.get('/list-closure-date', [verifyToken, isAdmin],listClosureDate)
 
 module.exports = router
