67aa96b73c2312af09eedeb17d5a58fc1d659088
nguyentiendat12a8 <74580065+nguyentiendat12a8@users.noreply.github.com>
2022-03-24 21:55:09 +0700

    update bug ideas router

--- src/routers/ideas.router.js
+++ src/routers/ideas.router.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { createIdeas, likeIdeas, dislikeIdeas, commentIdeas, listCommentIdeas, listIdeas,
-    viewSubmitIdeas, downloadIdeas, deleteCommentIdeas,  filter,
+    viewDetailIdeas, downloadIdeas, deleteCommentIdeas,  filter,
      myIdeas, 
      downloadZip} = require('../controllers/ideas.controller');
 const { checkClosureDate } = require('../middlerwares/checkClosureDate.middleware');
@@ -14,13 +14,13 @@ router.post('/upload-ideas', [verifyToken, upload.single('ideasFile')], createId
 router.post('/like-ideas/:ideasID', [verifyToken, checkClosureDate, checkLike], likeIdeas)
 router.post('/dislike-ideas/:ideasID', [verifyToken, checkClosureDate, checkDislike], dislikeIdeas)
 router.post('/comment-ideas/:ideasID', [verifyToken, checkClosureDate], commentIdeas)
-router.post('/delete-comment/:commnetID', deleteCommentIdeas)
+//router.post('/delete-comment/:commnetID', deleteCommentIdeas)
 
 //router.get('/list-comment-ideas/:ideasID', [verifyToken], listCommentIdeas)
 router.get('/list-ideas', [verifyToken], listIdeas)
 router.get('/list-my-ideas', [verifyToken], myIdeas)
 
-router.get('/view-detail-ideas/:ideasID', [verifyToken], viewSubmitIdeas)
+router.get('/view-detail-ideas/:ideasID', [verifyToken], viewDetailIdeas)
 
 router.get('/download-ideas', [verifyToken, isQA], downloadIdeas)
 router.get('/download-zip', downloadZip)
