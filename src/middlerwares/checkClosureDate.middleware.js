8c15712f83fe3dcd7b43c7d12248c6addf8a469b
nguyentiendat12a8 <74580065+nguyentiendat12a8@users.noreply.github.com>
2022-04-06 16:52:13 +0700

    update check date

--- src/middlerwares/checkClosureDate.middleware.js
+++ src/middlerwares/checkClosureDate.middleware.js
@@ -44,54 +44,41 @@ exports.checkFinalClosureDate = async (req,res,next) => {
 
 exports.checkFirstClosureDate = async (req,res,next) => {
     try {
-        //const d = new Date()
+        const d = new Date()
         const department = await Department.findOne({departmentName: req.body.departmentName})
         if(department === null) return res.status(500).send({
             errorCode: 500,
             message: 'department error'
         })
         const closureDate = await ClosureDate.findOne({departmentID: department._id})
-        if(new Date(closureDate.firstClosureDate) < new Date()){
-            return res.status(400).send({
-                errorCode: 400,
+        const date = closureDate.firstClosureDate.split('-') 
+
+        if (parseInt(date[2]) > parseInt(d.getFullYear())) {
+            next()
+        } else if (parseInt(date[2]) === parseInt(d.getFullYear())) {
+            if (parseInt(date[1]) > (parseInt(d.getMonth()) + 1)) {
+                next()
+            } else if (parseInt(date[1]) === (parseInt(d.getMonth()) + 1)) {
+                if (parseInt(date[0]) > parseInt(d.getDate())) {
+                    next()
+                } else {
+                    return res.status(401).send({
+                        errorCode: 401,
+                        message: 'Time is expired!'
+                    })
+                }
+            } else {
+                return res.status(401).send({
+                    errorCode: 401,
+                    message: 'Time is expired!'
+                })
+            }
+        } else {
+            return res.status(401).send({
+                errorCode: 401,
                 message: 'Time is expired!'
             })
         }
-        next()
-
-
-
-
-
-
-        // const date = closureDate.firstClosureDate.split('-') //await closureDate.finalClosureDate.split('/')
-
-        // if (parseInt(date[2]) > parseInt(d.getFullYear())) {
-        //     next()
-        // } else if (parseInt(date[2]) === parseInt(d.getFullYear())) {
-        //     if (parseInt(date[1]) > (parseInt(d.getMonth()) + 1)) {
-        //         next()
-        //     } else if (parseInt(date[1]) === (parseInt(d.getMonth()) + 1)) {
-        //         if (parseInt(date[0]) > parseInt(d.getDate())) {
-        //             next()
-        //         } else {
-        //             return res.status(401).send({
-        //                 errorCode: 401,
-        //                 message: 'Time is expired!'
-        //             })
-        //         }
-        //     } else {
-        //         return res.status(401).send({
-        //             errorCode: 401,
-        //             message: 'Time is expired!'
-        //         })
-        //     }
-        // } else {
-        //     return res.status(401).send({
-        //         errorCode: 401,
-        //         message: 'Time is expired!'
-        //     })
-        // }
     }
     catch (err) {
         console.log(err)
