e11673237e602d6a395bbfb7c3669b70492d59ed
nguyentiendat12a8 <74580065+nguyentiendat12a8@users.noreply.github.com>
2022-04-13 13:37:37 +0700

    update mail

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -5,7 +5,7 @@ const sendEmail = async (email, subject, text) => {
     try {
         const transporter = nodemailer.createTransport({
             //host: 'smtp.ethereal.email',
-            host: "smtp.ethereal.email",
+            //host: "smtp.ethereal.email",
             service: 'gmail',
             //port: 588,
             //secure: false,
