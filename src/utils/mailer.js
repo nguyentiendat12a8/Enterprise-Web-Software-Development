94294a9d5cb94d6f0a61fd2f86ffcde998564480
nguyentiendat12a8 <74580065+nguyentiendat12a8@users.noreply.github.com>
2022-04-13 14:09:02 +0700

    mail

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -1,31 +1,53 @@
 const nodemailer = require("nodemailer");
 
 const sendEmail = async (email, subject, text) => {
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.USER || 'abc@gmail.com', // TODO: your gmail account
+            pass: process.env.PASS || '1234' // TODO: your gmail password
+        }
+    });
     
-    try {
-        const transporter = nodemailer.createTransport({
-            //host: 'smtp.gmail.com',
-            host: "smtp.gmail.com",
-            service: 'gmail',
-            port: 587,
-            //secure: false,
-            auth: {
-                user: process.env.USER,
-                pass: process.env.PASS,
-            },
-        });
+    // Step 2
+    let mailOptions = {
+        from: process.env.USER, // TODO: email sender
+        to: 'nguyentiendat12a8@gmail.com', // TODO: email receiver
+        subject: 'Nodemailer - Test',
+        text: 'Wooohooo it works!!'
+    };
+    
+    // Step 3
+    transporter.sendMail(mailOptions, (err, data) => {
+        if (err) {
+            return console.log('Error occurs');
+        }
+        return console.log('Email sent!!!');
+    });
+    // try {
+    //     const transporter = nodemailer.createTransport({
+    //         //host: 'smtp.gmail.com',
+    //         host: "smtp.gmail.com",
+    //         service: 'gmail',
+    //         port: 587,
+    //         //secure: false,
+    //         auth: {
+    //             user: process.env.USER,
+    //             pass: process.env.PASS,
+    //         },
+    //     });
 
-        await transporter.sendMail({
-            from: process.env.USER,
-            to: email,
-            subject: subject,
-            text: text,
-        });
+    //     await transporter.sendMail({
+    //         from: process.env.USER,
+    //         to: email,
+    //         subject: subject,
+    //         text: text,
+    //     });
 
-        console.log("email sent sucessfully");
-    } catch (error) {
-        console.log(error, "email not sent");
-    }
+    //     console.log("email sent sucessfully");
+    // } catch (error) {
+    //     console.log(error, "email not sent");
+    // }
 };
 
 module.exports = sendEmail;
